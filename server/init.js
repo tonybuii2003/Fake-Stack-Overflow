fdd9d8f878946052c30d23eaa0aa3cdba2dd25a2
vitaliy <ryanstusalitusEPK@gmail.com>
2024-04-17 04:32:42 -0400

    feat: welcome page UI (login/register/enter as guest), functionality WIP

---
+++ server/init.js
@@ -0,0 +1,9 @@
+/*
+TODO:
+Include a script called server/init.js in your repository that will be run to create initial data in your schema. 
+This script will be run using Node before starting the server. 
+
+The initial data must contain the user profile for admin, so the username and password for an admin user 
+must be provided as the first argument to server/init.js. 
+You must use these credentials to create a user profile for admin in the database.
+*/
---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+// User Document Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    user_name: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    reputation: [{ type: Number, default: 0}],
+    creation_date: [{ type: Date, default: Date.now }]
+  });
+  userSchema.virtual('url').get(function () {
+    return `posts/users/${this._id}`;
+  });
+
+  module.exports = mongoose.model('User', userSchema);
